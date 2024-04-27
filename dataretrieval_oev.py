f4646aa5f35cce1768de4f2b17e14bce9c22e2cf
Christian <schweizerchristian50@gmail.com>
2024-04-27 10:51:24 +0200

    sfdsdf

---
+++ dataretrieval_oev.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+df = pd.read_csv('./oev_haltestellen/Betriebspunkt.csv', encoding='ANSI')
+
+print(df)
--- oev_haltestellen/dataretrieval_oev.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv('Betriebspunkt.csv')
-
-print(df)
